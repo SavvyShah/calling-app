5969538aeaf08bc4b8bbb0beeac05e91f1b7ffb0
Shubham Shah <shubhamshahrising@gmail.com>
2024-01-03 23:43:37 +0530

    fix: active link indicator

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,13 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Icon from "./styled-components/Icon";
+import classNames from "classnames";
 
 export const Footer = () => {
+  const location = useLocation();
+  const currentPath = location.pathname || "/";
+
   return (
     <ul className="z-1 h-18 border border-gray-100 bg-white max-w-md w-full p-2 flex flex-wrap justify-around text-sm font-medium text-center">
       <li>
         <Link
           to="/"
-          className="inline-block px-4 py-3 text-white bg-green-600 rounded-lg active"
+          className={classNames("inline-block px-4 py-3 rounded-lg active", {
+            "bg-green-600 text-white": currentPath === "/",
+          })}
           aria-current="page"
         >
           <Icon name="home" />
